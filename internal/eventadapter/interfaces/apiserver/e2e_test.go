2ae945aed381aed4a7f0f321d6874c78e1cee75e
UsadaPeko <usada@pekora.dev>
2022-05-07 01:25:37 +0900

    refactoring test code

--- internal/eventadapter/interfaces/apiserver/e2e_test.go
+++ internal/eventadapter/interfaces/apiserver/e2e_test.go
@@ -25,6 +25,26 @@ var _ = Describe("API Server", func() {
 	})
 
 	Context("Call POST root/service/{ServiceKey}/event", func() {
+		When("With Invalid Service Key", func() {
+			invalidServiceKey := "InvalidServiceKey"
+			jsonObject := map[string]interface{}{
+				"event_id":   "c5d7c94f-0fcf-4791-bc45-06d8fecbd356",
+				"event_name": "login",
+				"event_time": "2022-01-01T00:00:00Z",
+
+				"user_id": "usada pekora",
+			}
+			requestBody, err := json.Marshal(jsonObject)
+			Expect(err).Should(Succeed())
+
+			request := httptest.NewRequest("POST", "/service/"+invalidServiceKey+"/event", bytes.NewBuffer(requestBody))
+			It("Return 401", func() {
+				response, err := app.Test(request)
+				Expect(response.StatusCode).Should(Equal(http.StatusUnauthorized))
+				Expect(err).Should(Succeed())
+			})
+		})
+
 		When("Without user id", func() {
 			jsonObject := map[string]interface{}{
 				"event_id":   "c5d7c94f-0fcf-4791-bc45-06d8fecbd356",
@@ -41,6 +61,7 @@ var _ = Describe("API Server", func() {
 				Expect(err).Should(Succeed())
 			})
 		})
+
 		When("With login event", func() {
 			jsonObject := map[string]interface{}{
 				"event_id":   "c5d7c94f-0fcf-4791-bc45-06d8fecbd356",
@@ -59,25 +80,6 @@ var _ = Describe("API Server", func() {
 				Expect(err).Should(Succeed())
 			})
 		})
-	})
 
-	Context("Call POST root/service/{ServiceKey}/event with Invalid Service Key", func() {
-		invalidServiceKey := "InvalidServiceKey"
-		jsonObject := map[string]interface{}{
-			"event_id":   "c5d7c94f-0fcf-4791-bc45-06d8fecbd356",
-			"event_name": "login",
-			"event_time": "2022-01-01T00:00:00Z",
-
-			"user_id": "usada pekora",
-		}
-		requestBody, err := json.Marshal(jsonObject)
-		Expect(err).Should(Succeed())
-
-		request := httptest.NewRequest("POST", "/service/"+invalidServiceKey+"/event", bytes.NewBuffer(requestBody))
-		It("Return 401", func() {
-			response, err := app.Test(request)
-			Expect(response.StatusCode).Should(Equal(http.StatusUnauthorized))
-			Expect(err).Should(Succeed())
-		})
 	})
 })
