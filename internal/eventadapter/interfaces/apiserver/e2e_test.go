e61c87452ad8b8d9d5aa9f09c42d6e0a829d6f1a
UsadaPeko <usada@pekora.dev>
2022-05-07 01:15:50 +0900

    make new test

--- internal/eventadapter/interfaces/apiserver/e2e_test.go
+++ internal/eventadapter/interfaces/apiserver/e2e_test.go
@@ -42,4 +42,24 @@ var _ = Describe("API Server", func() {
 			Expect(err).Should(Succeed())
 		})
 	})
+
+	Context("Call POST root/service/{ServiceKey}/event with Invalid Service Key", func() {
+		invalidServiceKey := "InvalidServiceKey"
+		jsonObject := map[string]interface{}{
+			"event_id":   "c5d7c94f-0fcf-4791-bc45-06d8fecbd356",
+			"event_name": "login",
+			"event_time": "2022-01-01T00:00:00Z",
+
+			"user_id": "usada pekora",
+		}
+		requestBody, err := json.Marshal(jsonObject)
+		Expect(err).Should(Succeed())
+
+		request := httptest.NewRequest("POST", "/service/"+invalidServiceKey+"/event", bytes.NewBuffer(requestBody))
+		It("Return 200", func() {
+			response, err := app.Test(request)
+			Expect(response.StatusCode).Should(Equal(http.StatusUnauthorized))
+			Expect(err).Should(Succeed())
+		})
+	})
 })
