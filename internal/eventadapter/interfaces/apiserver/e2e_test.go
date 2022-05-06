f037e2137416925eacf149299a8d1928c80b6b49
UsadaPeko <usada@pekora.dev>
2022-05-07 01:09:06 +0900

    Implement Event Adapter API

--- internal/eventadapter/interfaces/apiserver/e2e_test.go
+++ internal/eventadapter/interfaces/apiserver/e2e_test.go
@@ -6,6 +6,8 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 )
@@ -15,6 +17,25 @@ var _ = Describe("API Server", func() {
 
 	Context("Call GET root/", func() {
 		request := httptest.NewRequest("GET", "/", nil)
+		It("Return 200", func() {
+			response, err := app.Test(request)
+			Expect(response.StatusCode).Should(Equal(http.StatusOK))
+			Expect(err).Should(Succeed())
+		})
+	})
+
+	Context("Call POST root/service/{ServiceKey}/event", func() {
+		jsonObject := map[string]interface{}{
+			"event_id":   "c5d7c94f-0fcf-4791-bc45-06d8fecbd356",
+			"event_name": "login",
+			"event_time": "2022-01-01T00:00:00Z",
+
+			"user_id": "usada pekora",
+		}
+		requestBody, err := json.Marshal(jsonObject)
+		Expect(err).Should(Succeed())
+
+		request := httptest.NewRequest("POST", "/service/c5d7c94f/event", bytes.NewBuffer(requestBody))
 		It("Return 200", func() {
 			response, err := app.Test(request)
 			Expect(response.StatusCode).Should(Equal(http.StatusOK))
