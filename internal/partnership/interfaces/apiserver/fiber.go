8791c95c7d9e7607d43e2eeb6f45c6c7a6a2e6a1
UsadaPeko <usada@pekora.dev>
2022-05-08 15:56:28 +0900

    green

--- internal/partnership/interfaces/apiserver/fiber.go
+++ internal/partnership/interfaces/apiserver/fiber.go
@@ -17,7 +17,7 @@ func MakeServer() *fiber.App {
 	})
 
 	app.Post("/partnership", func(ctx *fiber.Ctx) error {
-		return ctx.SendStatus(http.StatusCreated)
+		return ctx.Status(http.StatusCreated).SendString(`{"id": "7648d8e3-a31b-44a4-9d3b-f30679d4f3b6"}`)
 	})
 	return app
 }
